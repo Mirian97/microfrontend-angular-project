dad9c663c71c2765bbf520a6bd61aa5602683b62
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-10-12 16:24:17 -0300

    wip in home book component

---
+++ projects/mfe-app/src/app/book/home-book/home-book.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-book',
+  templateUrl: './home-book.component.html',
+  styleUrl: './home-book.component.scss'
+})
+export class HomeBookComponent {
+
+}
